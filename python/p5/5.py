1fc9a73b9f5bc05c4dd34fbdcf3ba10a11652dc2
SILES-WEB <87202617+SILES-WEB@users.noreply.github.com>
2023-12-06 20:38:27 +0700

    Delete python directory

--- python/p5/5.py
+++
@@ -1,8 +0,0 @@
-size = int(input('Введите размер таблицы: '))
-for row in range(1, size + 1):
-    for col in range(1, size + 1):
-        if row % 2 == 0:
-            print(row, end='\t')
-        else:
-            print(col, end='\t')
-    print()
